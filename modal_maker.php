b828d409587a38cb0a488f2fd592277eb5d04942
souravd000 <86484577+souravd000@users.noreply.github.com>
2024-11-11 13:30:29 +0530

    Update modal_maker.php
    V1.3

--- modal_maker.php
+++ modal_maker.php
@@ -3,33 +3,33 @@
  * Plugin Name: Modal Maker
  * Plugin URI: https://github.com/souravd000/modal-maker
  * Description: Create customizable modal popups in Elementor with ease, featuring flexible content options like button groups or rich text editors.
- * Version: 1.2
+ * Version: 1.3
  * Author: souravd000
  * License: GPL v2 or later
  * Text Domain: modal-maker 
  */
 
 // Define plugin version
-define( 'mm', '1.1' );
-
 if ( ! defined( 'ABSPATH' ) ) {
     exit; // Exit if accessed directly.
 }
+define( 'modalm', '1.3' );
+
 
 // Enqueue necessary scripts and styles
-function mm_widget_scripts() {
+function modalm_widget_scripts() {
     // Use MODAL_MAKER_VERSION to ensure correct versioning
-    wp_enqueue_style( 'my-modal-style', plugins_url( '/assets/css/my-modal-style.css', __FILE__ ), [], mm );
-    wp_enqueue_script( 'my-modal-script', plugins_url( '/assets/js/my-modal-script.js', __FILE__ ), [ 'jquery' ], mm, true );
+    wp_enqueue_style( 'my-modal-style', plugins_url( '/assets/css/my-modal-style.css', __FILE__ ), [], modalm );
+    wp_enqueue_script( 'my-modal-script', plugins_url( '/assets/js/my-modal-script.js', __FILE__ ), [ 'jquery' ], modalm, true );
 }
-add_action( 'wp_enqueue_scripts', 'mm_widget_scripts' );
+add_action( 'wp_enqueue_scripts', 'modalm_widget_scripts' );
 
 
 // Load the custom widget class.
-function mm_widget_init() {
+function modalm_widget_init() {
     // Check if Elementor is active and loaded.
     if ( did_action( 'elementor/loaded' ) ) {
         require_once __DIR__ . '/widgets/my-widget.php';
     }
 }
-add_action( 'elementor/widgets/register', 'mm_widget_init' );
+add_action( 'elementor/widgets/register', 'modalm_widget_init' );
